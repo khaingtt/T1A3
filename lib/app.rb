8ecc799ae513c0681b1405572b4796d0c1274f69
khaingtt <khaingtt@outlook.com>
2021-05-20 14:22:07 +1000

    add titlebar

--- lib/app.rb
+++ lib/app.rb
@@ -43,4 +43,18 @@ class App
     ]
     choice = @prompt.select("Please choose from the following options.\n\n", choices, help: "(Choose using ↑/↓ arrow keys, press Enter to select)", show_help: :always, per_page: 10)
   end
+
+  def titlebar
+    puts "-------------- Khai Reminder App --------------\n".colorize(:color => :yellow).underline
+  end
+#Reminder menu for the app
+  def reminder_weekly_input
+    reminder_name = @prompt.ask("What is the name of the reminder?", required: :true)
+    reminder_owner = @prompt.ask("Who is this reminder for?", required: :true)
+    choices = %w(Sunday Monday Tuesday Wednesday Thursday Friday Saturday)
+    reminder_days_taken = @prompt.multi_select("Which days of the week do you take it?", choices, per_page: 7, help: "\n(Press ↑/↓ arrow keys to navigate, Space to select and Enter to continue)", show_help: :always, min: 1)
+    reminder_times_taken = time_input
+    return reminder_name, reminder_owner, reminder_days_taken, reminder_times_taken
+  end
+  
 end
