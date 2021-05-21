a7d11d81e4ea1e901ab2ffecbf42fd43ba4688fc
khaingtt <khaingtt@outlook.com>
2021-05-21 22:16:11 +1000

    create ascii art to use intro and exit of the app

---
+++ lib/text.rb
@@ -0,0 +1,73 @@
+# This module call the text whenever I need to put text within the app
+module AppText
+
+require 'colorize'
+
+module_function
+
+# These are all the text files used throughout the app
+def title 
+"                                                                                                                                              
+                                                                                                                                                    
+KKKKKKKKK    KKKKKKKhhhhhhh                                 iiii                           AAA                                                      
+K:::::::K    K:::::Kh:::::h                                i::::i                         A:::A                                                     
+K:::::::K    K:::::Kh:::::h                                 iiii                         A:::::A                                                    
+K:::::::K   K::::::Kh:::::h                                                             A:::::::A                                                   
+KK::::::K  K:::::KKK h::::h hhhhh         aaaaaaaaaaaaa   iiiiiii                      A:::::::::A          ppppp   ppppppppp   ppppp   ppppppppp   
+  K:::::K K:::::K    h::::hh:::::hhh      a::::::::::::a  i:::::i                     A:::::A:::::A         p::::ppp:::::::::p  p::::ppp:::::::::p  
+  K::::::K:::::K     h::::::::::::::hh    aaaaaaaaa:::::a  i::::i                    A:::::A A:::::A        p:::::::::::::::::p p:::::::::::::::::p 
+  K:::::::::::K      h:::::::hhh::::::h            a::::a  i::::i                   A:::::A   A:::::A       pp::::::ppppp::::::ppp::::::ppppp::::::p
+  K:::::::::::K      h::::::h   h::::::h    aaaaaaa:::::a  i::::i                  A:::::A     A:::::A       p:::::p     p:::::p p:::::p     p:::::p
+  K::::::K:::::K     h:::::h     h:::::h  aa::::::::::::a  i::::i                 A:::::AAAAAAAAA:::::A      p:::::p     p:::::p p:::::p     p:::::p
+  K:::::K K:::::K    h:::::h     h:::::h a::::aaaa::::::a  i::::i                A:::::::::::::::::::::A     p:::::p     p:::::p p:::::p     p:::::p
+KK::::::K  K:::::KKK h:::::h     h:::::ha::::a    a:::::a  i::::i               A:::::AAAAAAAAAAAAA:::::A    p:::::p    p::::::p p:::::p    p::::::p
+K:::::::K   K::::::K h:::::h     h:::::ha::::a    a:::::a i::::::i             A:::::A             A:::::A   p:::::ppppp:::::::p p:::::ppppp:::::::p
+K:::::::K    K:::::K h:::::h     h:::::ha:::::aaaa::::::a i::::::i            A:::::A               A:::::A  p::::::::::::::::p  p::::::::::::::::p 
+K:::::::K    K:::::K h:::::h     h:::::h a::::::::::aa:::ai::::::i           A:::::A                 A:::::A p::::::::::::::pp   p::::::::::::::pp  
+KKKKKKKKK    KKKKKKK hhhhhhh     hhhhhhh  aaaaaaaaaa  aaaaiiiiiiii          AAAAAAA                   AAAAAAAp::::::pppppppp     p::::::pppppppp    
+                                                                                                             p:::::p             p:::::p            
+                                                                                                             p:::::p             p:::::p            
+                                                                                                            p:::::::p           p:::::::p           
+                                                                                                            p:::::::p           p:::::::p           
+                                                                                                            p:::::::p           p:::::::p           
+                                                                                                            ppppppppp           ppppppppp           
+                                                                                                                                                                                                        
+"
+end
+
+def any_key
+    puts "
+    Press any key to continue...
+    ".yellow
+end
+
+def exit
+"
+                                                                                                                                                                
+                                                                                                                                                                
+TTTTTTTTTTTTTTTTTTTTTTThhhhhhh                                                kkkkkkkk                                                                          
+T:::::::::::::::::::::Th:::::h                                                k::::::k                                                                          
+T:::::::::::::::::::::Th:::::h                                                k::::::k                                                                          
+T:::::TT:::::::TT:::::Th:::::h                                                k::::::k                                                                          
+TTTTTT  T:::::T  TTTTTT h::::h hhhhh         aaaaaaaaaaaaa  nnnn  nnnnnnnn     k:::::k    kkkkkkk     yyyyyyy           yyyyyyy ooooooooooo   uuuuuu    uuuuuu  
+        T:::::T         h::::hh:::::hhh      a::::::::::::a n:::nn::::::::nn   k:::::k   k:::::k       y:::::y         y:::::yoo:::::::::::oo u::::u    u::::u  
+        T:::::T         h::::::::::::::hh    aaaaaaaaa:::::an::::::::::::::nn  k:::::k  k:::::k         y:::::y       y:::::yo:::::::::::::::ou::::u    u::::u  
+        T:::::T         h:::::::hhh::::::h            a::::ann:::::::::::::::n k:::::k k:::::k           y:::::y     y:::::y o:::::ooooo:::::ou::::u    u::::u  
+        T:::::T         h::::::h   h::::::h    aaaaaaa:::::a  n:::::nnnn:::::n k::::::k:::::k             y:::::y   y:::::y  o::::o     o::::ou::::u    u::::u  
+        T:::::T         h:::::h     h:::::h  aa::::::::::::a  n::::n    n::::n k:::::::::::k               y:::::y y:::::y   o::::o     o::::ou::::u    u::::u  
+        T:::::T         h:::::h     h:::::h a::::aaaa::::::a  n::::n    n::::n k:::::::::::k                y:::::y:::::y    o::::o     o::::ou::::u    u::::u  
+        T:::::T         h:::::h     h:::::ha::::a    a:::::a  n::::n    n::::n k::::::k:::::k                y:::::::::y     o::::o     o::::ou:::::uuuu:::::u  
+        TT:::::::TT       h:::::h     h:::::ha::::a    a:::::a  n::::n    n::::nk::::::k k:::::k                y:::::::y      o:::::ooooo:::::ou:::::::::::::::uu
+        T:::::::::T       h:::::h     h:::::ha:::::aaaa::::::a  n::::n    n::::nk::::::k  k:::::k                y:::::y       o:::::::::::::::o u:::::::::::::::u
+        T:::::::::T       h:::::h     h:::::h a::::::::::aa:::a n::::n    n::::nk::::::k   k:::::k              y:::::y         oo:::::::::::oo   uu::::::::uu:::u
+        TTTTTTTTTTT       hhhhhhh     hhhhhhh  aaaaaaaaaa  aaaa nnnnnn    nnnnnnkkkkkkkk    kkkkkkk            y:::::y            ooooooooooo       uuuuuuuu  uuuu
+                                                                                                            y:::::y                                             
+                                                                                                            y:::::y                                              
+                                                                                                            y:::::y                                               
+                                                                                                            y:::::y                                                
+                                                                                                        yyyyyyy                                                 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+"
+end
+
+end
