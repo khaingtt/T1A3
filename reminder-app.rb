f16a252d0931f8016a9fb0b1b8bddf26ac99ab2c
khaingtt <khaingtt@outlook.com>
2021-05-21 23:13:14 +1000

    use ascii art class to display

--- reminder-app.rb
+++ reminder-app.rb
@@ -1,8 +1,13 @@
 require_relative ("lib/app.rb")
 require_relative ("lib/reminder.rb")
+require_relative ("lib/text.rb")
 
 reminderapp = App.new
 
+# This is just the welcome to the app then it calls the method to start the app
+puts "#{AppText.title}".blue
+prompt = TTY::Prompt.new
+prompt.keypress(AppText.any_key)
 
 
 reminderapp.run
