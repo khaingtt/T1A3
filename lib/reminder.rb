8bc48302008df20e0375289ebad877c0e1c85625
khaingtt <khaingtt@outlook.com>
2021-05-24 12:03:25 +1000

    add next 24 hours function in reminderweekly class

--- lib/reminder.rb
+++ lib/reminder.rb
@@ -11,6 +11,7 @@ class Reminder
     def initialize(name)
         @name = name
     end
+end
 
 class ReminderWeekly < Reminder
     attr_accessor :days_taken, :times_taken
@@ -49,6 +50,20 @@ class ReminderWeekly < Reminder
         puts "\n"
       end
       
+      def next_hours(hours)
+        start = Time.now
+        final = start + (hours * 3600)
+        reminders_to_take = @times_taken.filter do |time_stamp|
+          if time_stamp[:hour].to_i > start.hour
+            time_stamp = Time.new(start.year, start.month, start.day, time_stamp[:hour].to_i, time_stamp[:minute].to_i)
+          else
+            time_stamp = Time.new(start.year, start.month, start.day + 1, time_stamp[:hour].to_i, time_stamp[:minute].to_i)
+          end
+          @days_taken.include?(time_stamp.strftime("%A")) && time_stamp >= start && time_stamp <= final
+        end
+        reminders_to_take.each do |time|
+          puts "\n#{@name} (#{owner})".colorize(:magenta) + " at " + "#{time[:hour]}:#{time[:minute]}".colorize(:yellow) + "\n"
+        end
+      end
 end
 
-end
